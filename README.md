a906a6442d8ca7163bdb94957cccf0703952242a
Tzeng Yuxio <tzengyuxio@gmail.com>
2023-02-05 16:10:07 +0800

    feat: use new way to export faces

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # kaodata
 Early KOEI Games Data Research
+
+## Faces 頭像
+
+| ID | 中文名 | 日文名 | 年份 | 檔案 | 尺寸 | 數量 | Memo |
+|--|--|--|--|--|--|--|--|--|
+| TK2 | 提督之決斷II | 提督の決断II | 1993(DOS) | `KAO.TK2` | 48x64 | 103 | |
+| EUROPE | 歐陸戰線 | ヨーロッパ戦線 | 1994(DOS) | `FACE.DAT` | 64x80 | 120 | 特色是有白邊與陰影 |
+
+
+## Fonts 字庫
--- kaodata.py
+++ kaodata.py
@@ -64,22 +64,36 @@ KHAN_PALETTE = ['#302000', '#417120', '#D33030', '#D3B282', '#204182', '#418292'
                 '#D3D3B2']  # 黑 綠 紅 粉 藍 青 橙 白
 
 # 歐陸戰線
-EUROPA_KAODATA = "/Users/tzengyuxio/DOSBox/OLZX/FACE.DAT"
-EUROPA_PALETTE = ['#000000', '#419241', '#B24120', '#F3C361', '#104192', '#6FAEAE', '#D371B2',
-                  '#F3F3F3']  # 黑 綠 紅 粉 藍 青 橙 白
+EUROPE_KAODATA = "/Users/tzengyuxio/DOSBox/OLZX/FACE.DAT"
+EUROPE_PALETTE = ['#000000', '#419241', '#B24120', '#F3C361', '#104192', '#6FAEAE', '#D371B2', '#F3F3F3']
 
 # 提督的決斷2
 TK2_KAODATA = "/Users/tzengyuxio/DOSBox/TK2/KAO.TK2"
 TK2_PALETTE = ['#000000', '#419241', '#B24120', '#F3C361', '#104192', '#6FAEAE', '#D371B2',
-                  '#F3F3F3']  # 黑 綠 紅 粉 藍 青 橙 白
+               '#F3F3F3']  # 黑 綠 紅 粉 藍 青 橙 白
 
 # 魔法皇冠
 GEMFIRE_KAODATA = "/Users/tzengyuxio/DOSBox/GEMFIRE/KAODATA.DAT"
 GEMFIRE_PALETTE = ['#302000', '#417120', '#B24120', '#D3B282', '#204182', '#418292', '#C38251',
                    '#D3D3B2']  # 黑 綠 紅 粉 藍 青 橙 白
 
+GAME_INFOS = {
+    "EUROPE": {
+        "name": "歐陸戰線",
+        "face_file": "FACE.DAT",
+        "face_size": (64, 80),
+        "palette": ['#000000', '#419241', '#B24120', '#F3C361', '#104192', '#6FAEAE', '#D371B2', '#F3F3F3']
+    },
+    "TK2": {
+        "name": "提督之決斷II",
+        "face_file": "KAO.TK2",
+        "face_size": (48, 64),
+        "palette": ['#000000', '#417100', '#D32000', '#E3A261', '#0030A2', '#7192B2', '#C36161', '#F3F3F3']
+    }
+}
 
-def convert_to_array1(data_bytes):
+
+def convert_to_array_4color(data_bytes):
     array = []
     it = iter(data_bytes)
     for b1 in it:
@@ -90,7 +104,7 @@ def convert_to_array1(data_bytes):
     return array
 
 
-def convert_to_array(data_bytes):
+def convert_to_array_8color(data_bytes):
     array = []
     it = iter(data_bytes)
     for b1 in it:
@@ -99,6 +113,80 @@ def convert_to_array(data_bytes):
             n = ((b1 >> i) & 1) * 4 + ((b2 >> i) & 1) * 2 + ((b3 >> i) & 1)
             array.append(n)
     return array
+
+
+def convert_to_array(data_bytes, color_table):
+    return convert_to_array_4color(data_bytes) if len(color_table) == 4 else convert_to_array_8color(data_bytes)
+
+
+def bytes_to_image(data, w, h, color_table, dh=False):
+    """
+    Convert binary bytes to image.
+
+    :param data:        binary bytes
+    :param w:           width
+    :param h:           height
+    :param color_table: color table
+    :param dh:          double height
+    :return:            PIL.Image
+    """
+    img = Image.new('RGB', (w, h))
+    color_indexes = convert_to_array(data, color_table)
+    for idx, color_index in enumerate(color_indexes):
+        x, y = idx % w, idx // w
+        c = color_table[color_index]
+        if dh:
+            img.putpixel((x, 2 * y), c)
+            img.putpixel((x, 2 * y + 1), c)
+        else:
+            img.putpixel((x, y), c)
+    return img
+
+
+def export_faces(tag, path, all_in_one=False):
+    game_info = GAME_INFOS[tag]
+
+    color_table = []
+    for c in game_info['palette']:
+        rr, gg, bb = c[1:3], c[3:5], c[5:7]
+        color_table.append((int(rr, base=16), int(gg, base=16), int(bb, base=16)))
+
+    filename = path + '/' + game_info['face_file']
+    face_w, face_h = game_info['face_size']
+    data_size = int((face_w * face_h) * 3 / 8)  # TODO: replace '3' with color depth
+    images = []
+    with open(filename, 'rb') as f:
+        f.seek(0, os.SEEK_END)
+        file_size = f.tell()
+        num_face = file_size // data_size
+        print('  file size   : {}'.format(file_size))
+        print('  face size   : {}x{}, ({} bytes)'.format(face_w, face_h, data_size))
+        print('  num of faces: {}'.format(num_face))
+        f.seek(0)
+
+        while data_bytes := f.read(data_size):
+            images.append(bytes_to_image(data_bytes, face_w, face_h, color_table))
+
+    if not os.path.exists(tag):
+        os.makedirs(tag)
+
+    if all_in_one:
+        img_w = face_w * 16
+        img_h = face_h * ((num_face // 16) + 1)
+        back_image = Image.new('RGB', (img_w, img_h), color='black')
+        for idx, img in enumerate(images):
+            pos_x = (idx % 16) * face_w
+            pos_y = (idx // 16) * face_h
+            back_image.paste(img, (pos_x, pos_y))
+        out_filename = '{}/00_{}_FACES.png'.format(tag, tag)
+        back_image.save(out_filename)
+        print('...save {}'.format(out_filename))
+    else:
+        for idx, img in enumerate(images):
+            out_filename = '{}/{}_{:04d}.png'.format(tag, tag, idx + 1)
+            img.save(out_filename)
+            print('...save {}'.format(out_filename))
+        print('{} images of face saved in [{}]'.format(num_face, tag))
 
 
 def export_kaodata1(tag, filename, palette, stretch=False):
@@ -116,7 +204,7 @@ def export_kaodata1(tag, filename, palette, stretch=False):
         while data_bytes := f.read(read_size):
             i += 1
             img = Image.new('RGB', (48, 80))
-            color_codes = convert_to_array1(data_bytes)
+            color_codes = convert_to_array_4color(data_bytes)
             for idx, color_code in enumerate(color_codes):
                 x, y = idx % 48, idx // 48
                 # img.putpixel((x, y), color_table[color_code])
@@ -143,7 +231,7 @@ def export_kaodata(tag, filename, palette, stretch=False):
         while data_bytes := f.read(read_size):
             i += 1
             img = Image.new('RGB', (64, 80))
-            color_codes = convert_to_array(data_bytes)
+            color_codes = convert_to_array_8color(data_bytes)
             for idx, color_code in enumerate(color_codes):
                 x, y = idx % 64, idx // 64
                 if not stretch:
@@ -183,7 +271,7 @@ def export_single_kaodata(tag, filename, palette, stretch=False):
         img = Image.new('RGB', (img_w, img_h), color='black')
         f.seek(0)
         while data_bytes := f.read(face_size):
-            color_codes = convert_to_array(data_bytes)
+            color_codes = convert_to_array_8color(data_bytes)
             for idx, color_code in enumerate(color_codes):
                 x, y = idx % 64, idx // 64
                 abs_x, abs_y = (i % 16) * face_w + x, (i // 16) * face_h + y
@@ -291,12 +379,20 @@ def revert(array):
 # export_kaodata('KHAN', KHAN_KAODATA, KHAN_PALETTE)
 
 # 歐陸戰線
-export_kaodata('EUROPE', EUROPA_KAODATA, EUROPA_PALETTE)
-export_single_kaodata('EUROPE', EUROPA_KAODATA, EUROPA_PALETTE)
+# export_kaodata('EUROPE', EUROPE_KAODATA, EUROPE_PALETTE)
+# export_single_kaodata('EUROPE', EUROPE_KAODATA, EUROPE_PALETTE)
 
 # 提督的決斷2
-export_kaodata('TK2', TK2_KAODATA, TK2_PALETTE)
-export_single_kaodata('TK2', TK2_KAODATA, TK2_PALETTE)
+# export_kaodata('TK2', TK2_KAODATA, TK2_PALETTE)
+# export_single_kaodata('TK2', TK2_KAODATA, TK2_PALETTE)
 
 # 魔法皇冠
 # export_kaodata('GEMFIRE', GEMFIRE_KAODATA, GEMFIRE_PALETTE, stretch=True)
+
+# 歐陸戰線
+# export_faces('EUROPE', '/Users/tzengyuxio/DOSBox/OLZX')
+# export_faces('EUROPE', '/Users/tzengyuxio/DOSBox/OLZX', all_in_one=True)
+
+# 提督的決斷2
+export_faces('TK2', '/Users/tzengyuxio/DOSBox/TK2')
+export_faces('TK2', '/Users/tzengyuxio/DOSBox/TK2', all_in_one=True)
