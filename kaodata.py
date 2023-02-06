5e305035fa46092310b2e7e045b62f8c0e9c203f
Tzeng Yuxio <tzengyuxio@gmail.com>
2023-02-06 11:36:01 +0800

    feat: add 三國志, 三國志2

--- kaodata.py
+++ kaodata.py
@@ -4,24 +4,14 @@ from functools import reduce
 
 from PIL import Image
 
-#
-SAN1_KAODATA = "/Users/tzengyuxio/DOSBox/SAN1/SAN_B/PICDATA.DAT"
-SAN1_PALETTE = ['#000000', '#10B251', '#F35100', '#F3E300', '#0041F3', '#00C3F3', '#F351D3',
-                '#F3F3F3']  # 黑 綠 朱 黃 藍 青 洋紅 白
 
 #
-SAN2_KAODATA = "/Users/tzengyuxio/DOSBox/SAN2/KAODATA.DAT"
 SAN2_MSG16P = "/Users/tzengyuxio/DOSBox/SAN2/MSG.16P"
 SAN2_NAME16P = "/Users/tzengyuxio/DOSBox/SAN2/NAME.16P"
-SAN2_PALETTE = ['#000000', '#10B251', '#F35100', '#F3E300', '#0041F3', '#00C3F3', '#F351D3',
-                '#F3F3F3']  # 黑 綠 朱 黃 藍 青 洋紅 白
 
 # 307張
 SAN3_HAN16P = "/Users/tzengyuxio/DOSBox/SAN3/HAN.16P"  # 1335 字
 SAN3_NAME16P = "/Users/tzengyuxio/DOSBox/SAN3/NAME.16P"
-
-# 530,413 byte (340 人需要 652,800)
-SAN4_KAODATA = "/Users/tzengyuxio/DOSBox/SAN4/KAODATA.S4"
 
 # 古事記外傳 KAMI/FACEGRP.DAT: 23,040 = 12 * 1920 (不成相)
 KAMI_KAODATA = "/Users/tzengyuxio/DOSBox/KAMI/FACEGRP.DAT"
@@ -57,12 +47,20 @@ GAME_INFOS = {
         "face_size": (64, 80),
         "palette": ['#000000', '#418200', '#C34100', '#E3A251', '#0030A2', '#71A2B2', '#B27171', '#F3E3D3']
     },
-    "SAN1": {},
+    "SAN1": {
+        "name": "三國志",
+        "face_file": "SAN_B/PICDATA.DAT",
+        "face_size": (48, 80),
+        "face_count": 114,
+        "double_height": True,
+        "palette": ['#000000', '#55FF55', '#FF5555', '#FFFF55']
+    },
     "SAN2": {
         "name": "三國志II",
         "face_file": "KAODATA.DAT",
         "face_size": (64, 80),
-        "palette": ['#000000x', '#418200x', '#C34100x', '#E3A251x', '#0030A2x', '#71A2B2x', '#B27171x', '#F3E3D3x']
+        "double_height": True,
+        "palette": ['#000000', '#55FF55', '#FF5555', '#FFFF55', '#5555FF', '#55FFFF', '#FF55FF', '#FFFFFF']
     },
     "SAN3": {
         "name": "三國志III",
@@ -72,7 +70,7 @@ GAME_INFOS = {
     },
     "SAN4": {
         "name": "三國志IV",
-        "face_file": "KAODATAP.S4",
+        "face_file": "KAODATAP.S4", # KAODATA.S4 作用尚不明, File Size: 530,413 byte (340 人需要 652,800)
         "face_size": (64, 80),
         "palette": ['#302000', '#417120', '#B24120', '#D3B282', '#204182', '#418292', '#C38251', '#D3D3B2']
     },
@@ -94,6 +92,18 @@ GAME_INFOS = {
     "SAN5P": {
         "name": "三國志V 威力加強版",
         "face_file": "KAOEX.S5",
+        "face_size": (64, 80),
+        "palette": ['#202010', '#206510', '#BA3000', '#EFAA8A', '#104575', '#658A9A', '#BA7545', '#EFDFCF']
+    },
+    "AIR2": {
+        "name": "航空霸業II",
+        "face_file": "MAN.GDT", # CITYFACE.GDT, MAKFACE.GDT, MAKER.GDT, MAN.GDT, STAFF1.GDT 均非 KAO
+        "face_size": (64, 80),
+        "palette": ['#202010', '#206510', '#BA3000', '#EFAA8A', '#104575', '#658A9A', '#BA7545', '#EFDFCF']
+    },
+    "LIBERTY": {
+        "name": "獨立戰爭",
+        "face_file": "FACE.IDX",
         "face_size": (64, 80),
         "palette": ['#202010', '#206510', '#BA3000', '#EFAA8A', '#104575', '#658A9A', '#BA7545', '#EFDFCF']
     }
@@ -160,21 +170,28 @@ def export_faces(tag, path, all_in_one=False):
 
     filename = path + '/' + game_info['face_file']
     face_w, face_h = game_info['face_size']
-    data_size = int((face_w * face_h) * 3 / 8)  # TODO: replace '3' with color depth
+    dh = True if 'double_height' in game_info and game_info['double_height'] else False
+    face_count = -1 if 'face_count' not in game_info else game_info['face_count']
+    num_bits_per_color = 2 if len(color_table) == 4 else 3
+    data_size = int((face_w * face_h) * num_bits_per_color / 8)
+    if dh:
+        data_size = int(data_size / 2)
     images = []
     with open(filename, 'rb') as f:
         f.seek(0, os.SEEK_END)
         file_size = f.tell()
-        num_face = file_size // data_size
+        num_face = face_count if face_count > 0 else file_size // data_size
         print('  file size   : {}'.format(file_size))
         print('  face size   : {}x{}, ({} bytes)'.format(face_w, face_h, data_size))
         print('  num of faces: {}'.format(num_face))
         f.seek(0)
 
+        i = 0
         while data_bytes := f.read(data_size):
-            if len(data_bytes) < data_size:
+            if len(data_bytes) < data_size or i >= num_face:
                 break
-            images.append(bytes_to_image(data_bytes, face_w, face_h, color_table))
+            images.append(bytes_to_image(data_bytes, face_w, face_h, color_table, dh))
+            i += 1
 
     if not os.path.exists(tag):
         os.makedirs(tag)
@@ -196,33 +213,6 @@ def export_faces(tag, path, all_in_one=False):
             img.save(out_filename)
             print('...save {}'.format(out_filename))
         print('{} images of face saved in [{}]'.format(num_face, tag))
-
-
-def export_kaodata1(tag, filename, palette, stretch=False):
-    color_table = []
-    for c in palette:
-        rr, gg, bb = c[1:3], c[3:5], c[5:7]
-        color_table.append((int(rr, base=16), int(gg, base=16), int(bb, base=16)))
-
-    if not os.path.exists(tag):
-        os.makedirs(tag)
-
-    with open(filename, 'rb') as f:
-        i = 0
-        read_size = 480
-        while data_bytes := f.read(read_size):
-            i += 1
-            img = Image.new('RGB', (48, 80))
-            color_codes = convert_to_array_4color(data_bytes)
-            for idx, color_code in enumerate(color_codes):
-                x, y = idx % 48, idx // 48
-                # img.putpixel((x, y), color_table[color_code])
-                img.putpixel((x, 2 * y), color_table[color_code])
-                img.putpixel((x, 2 * y + 1), color_table[color_code])
-            img_filename = '{}/{}_{:04d}.png'.format(tag, tag, i)
-            img.save(img_filename)
-            print('...save {}'.format(img_filename))
-    print('{} images of face saved in [{}]'.format(i, tag))
 
 
 def export_kaodata(tag, filename, palette, stretch=False):
@@ -252,49 +242,6 @@ def export_kaodata(tag, filename, palette, stretch=False):
             img.save(img_filename)
             print('...save {}'.format(img_filename))
     print('{} images of face saved in [{}]'.format(i, tag))
-
-
-def export_single_kaodata(tag, filename, palette, stretch=False):
-    color_table = []
-    for c in palette:
-        rr, gg, bb = c[1:3], c[3:5], c[5:7]
-        color_table.append((int(rr, base=16), int(gg, base=16), int(bb, base=16)))
-
-    if not os.path.exists(tag):
-        os.makedirs(tag)
-
-    face_size = 1920 if not stretch else 960
-    face_w = 64
-    face_h = 80
-    with open(filename, 'rb') as f:
-        f.seek(0, os.SEEK_END)
-        file_size = f.tell()
-        num_face = file_size // face_size
-        img_w = face_w * 16
-        img_h = face_h * ((num_face // 16) + 1)
-        print('file size   : {}'.format(file_size))
-        print('num of fonts: {}'.format(num_face))
-        print('image size  : {}x{} ({}x{})'.format(img_w, img_h, 40, ((num_face // 40) + 1)))
-
-        i = 0
-        img = Image.new('RGB', (img_w, img_h), color='black')
-        f.seek(0)
-        while data_bytes := f.read(face_size):
-            color_codes = convert_to_array_8color(data_bytes)
-            for idx, color_code in enumerate(color_codes):
-                x, y = idx % 64, idx // 64
-                abs_x, abs_y = (i % 16) * face_w + x, (i // 16) * face_h + y
-                if not stretch:
-                    img.putpixel((abs_x, abs_y), color_table[color_code])
-                else:
-                    abs_y = (i // 16) * img_h + 2 * y
-                    img.putpixel((abs_x, abs_y), color_table[color_code])
-                    img.putpixel((abs_x, abs_y + 1), color_table[color_code])
-            i += 1
-        img_filename = '{}/{}_kaodata.png'.format(tag, tag)
-        img.save(img_filename)
-        print('...save {}'.format(img_filename))
-    print('{} images of face saved in [{}]'.format(num_face, tag))
 
 
 def export_font(tag, filename, font_h=14, pre=False):
@@ -356,20 +303,13 @@ def revert(array):
 
 # revert([0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 3, 0, 3, 2, 3, 2, 2])
 
-# 三國志1
-# export_kaodata1('SAN1', SAN1_KAODATA, SAN1_PALETTE)
-
 # 三國志2
-# export_kaodata('SAN2', SAN2_KAODATA, SAN2_PALETTE, stretch=True)
 # export_font('SAN2', SAN2_MSG16P, pre=True)
 # export_font('SAN2', SAN2_NAME16P)
 
 # 三國志3
 # export_font('SAN3', SAN3_HAN16P, font_h=14)
 # export_font('SAN3', SAN3_NAME16P, font_h=14)
-
-# 三國志4
-# export_kaodata('SAN4', SAN4_KAODATA, SAN4_PALETTE)
 
 # 古事記外傳 (色盤未確定)
 # export_kaodata('KAMI', KAMI_KAODATA, KAMI_PALETTE)
@@ -395,6 +335,10 @@ def revert(array):
 # export_faces('KOHRYUKI', '/Users/tzengyuxio/DOSBox/KANSO', all_in_one=True)
 
 # 三國志 1~5
+# export_faces('SAN1', '/Users/tzengyuxio/DOSBox/SAN1')
+# export_faces('SAN1', '/Users/tzengyuxio/DOSBox/SAN1', all_in_one=True)
+# export_faces('SAN2', '/Users/tzengyuxio/DOSBox/SAN2')
+# export_faces('SAN2', '/Users/tzengyuxio/DOSBox/SAN2', all_in_one=True)
 # export_faces('SAN3', '/Users/tzengyuxio/DOSBox/SAN3')
 # export_faces('SAN3', '/Users/tzengyuxio/DOSBox/SAN3', all_in_one=True)
 # export_faces('SAN4', '/Users/tzengyuxio/DOSBox/SAN4')
@@ -405,3 +349,10 @@ def revert(array):
 # export_faces('SAN5', '/Users/tzengyuxio/DOSBox/SAN5', all_in_one=True)
 # export_faces('SAN5P', '/Users/tzengyuxio/DOSBox/SAN5')
 # export_faces('SAN5P', '/Users/tzengyuxio/DOSBox/SAN5', all_in_one=True)
+
+# 航空霸業II (尚未找到)
+# export_faces('AIR2', '/Users/tzengyuxio/DOSBox/AIR2', all_in_one=True)
+
+# 獨立戰爭 (尚未找到)
+# export_faces('LIBERTY', '/Users/tzengyuxio/DOSBox/LIBERTY')
+export_faces('LIBERTY', '/Users/tzengyuxio/DOSBox/LIBERTY', all_in_one=True)
