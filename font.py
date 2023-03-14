8de2847a13bf64d1c06d95058df75ecbffd8c675
Tzeng Yuxio <tzengyuxio@gmail.com>
2023-03-15 07:28:01 +0800

    feat: add glyph table

--- font.py
+++ font.py
@@ -187,7 +187,7 @@ ctable = {
 
 
 def view_big5_code():
-    s = '輿謝旌薛悌禮田豐'  # '即晨晝夜晴雲多雨霧'
+    s = '即晨晝夜'  # '即晨晝夜晴雲多雨霧'
     for c in s:
         print(c, big5_code(c))
 
@@ -255,11 +255,11 @@ def draw_table():
         unique_lower_codes.add(c[2:])
     print('unique_lower_codes len: {:4d}'.format(len(unique_lower_codes)))
 
-    for c in sorted(adjusted_codes):
-        print(c)
-    print('------------------------------------------------------------------------------')
-    for c in sorted(uncommon_codes):
-        print(c)
+    # for c in sorted(adjusted_codes):
+    #     print(c)
+    # print('------------------------------------------------------------------------------')
+    # for c in sorted(uncommon_codes):
+    #     print(c)
     print('adjusted_codes:{}'.format(len(adjusted_codes)))
     print('uncommon_codes:{}'.format(len(uncommon_codes)))
 
@@ -304,6 +304,7 @@ def extract_font(filename: str, glyph_h: int = 14, prefix: str = '', has_big5_co
         [ ] Auto add pages when num_row is not enough
         [x] Output filename with count
         [ ] Output big5 code list as well
+        [ ] Fix total number of rich progress bar
     """
     file_size = os.stat(filename).st_size
     font_data_size = glyph_h * 2
@@ -324,32 +325,46 @@ def extract_font(filename: str, glyph_h: int = 14, prefix: str = '', has_big5_co
     cell_width = 20
     cell_height = 20
     num_col = 40
+    num_row_max = 11
     num_row = ceil(font_data_count / num_col)
-    img_w = cell_width * num_col
-    img_h = cell_height * num_row
-    img = Image.new('RGB', (img_w, img_h), color='white')
-    for glyph_idx, font_data in track(enumerate(font_data_list), total=font_data_count):
-        for byte_idx, byte in enumerate(font_data):
-            for k in range(7, -1, -1):
-                bit = (byte >> k) & 1
-                rel_x = 7-k+8*(byte_idx % 2)
-                rel_y = byte_idx // 2
-                abs_x = (glyph_idx % num_col) * cell_width + rel_x
-                abs_y = (glyph_idx // num_col) * cell_width + rel_y
-                color = (0, 0, 0) if bit == 1 else (255, 255, 255)
-                img.putpixel((abs_x, abs_y), color)
+    if len(font_data_list) > 2000:
+        num_row = num_row_max
+    count_per_page = num_col * num_row
+    pages = ceil(font_data_count / count_per_page)
+    for page_idx in range(pages):
+        img_w = cell_width * num_col
+        img_h = cell_height * num_row
+        img = Image.new('RGB', (img_w, img_h), color='white')
+        font_data_list_page = font_data_list[page_idx*count_per_page:(page_idx+1)*count_per_page]
+        total = len(font_data_list_page) % count_per_page if page_idx == pages-1 else count_per_page
+        for glyph_idx, font_data in track(enumerate(font_data_list_page), total=total):
+            for byte_idx, byte in enumerate(font_data):
+                for k in range(7, -1, -1):
+                    bit = (byte >> k) & 1
+                    rel_x = 7-k+8*(byte_idx % 2)
+                    rel_y = byte_idx // 2
+                    abs_x = (glyph_idx % num_col) * cell_width + rel_x
+                    abs_y = (glyph_idx // num_col) * cell_width + rel_y
+                    color = (0, 0, 0) if bit == 1 else (255, 255, 255)
+                    img.putpixel((abs_x, abs_y), color)
 
-    img.save('font_index_{}({}).png'.format(prefix, int(font_data_count)))
+        page_postfix = '_{:02d}'.format(page_idx+1) if pages > 1 else ''
+        font_index_filename = 'GLYPH_TABLE_{}({}){}.png'.format(prefix, int(font_data_count), page_postfix)
+        img.save(font_index_filename)
 
 # TODO:
 #   [ ] find out all the font files
 #   [ ] list all the font which koei-code is higher than A9
 #   [ ] OCR? https://gist.github.com/beremaran/dc41c96aa8e3aaa1c1951428314df554
+#   [ ] write function to convert koei-tw to big5 then to utf-8
+#   [ ] check the font order in ziku.16p or name.16p
+#       - and see if there is any pattern of the delta between koei-tw and big5
+#       - and see if all game use the same font table
 
 
-# view_big5_code()
-list_relation_and_verify()
-# draw_table()
+view_big5_code()
+# list_relation_and_verify()
+draw_table()
 
 
 # == Extract font from file ==
@@ -359,7 +374,9 @@ list_relation_and_verify()
 # extract_font(EUROPE_MSG16P, 14, 'EUROPE_MSG16P')
 # extract_font(EUROPE_EMSG16P, 14, 'EUROPE_EMSG16P')
 # extract_font(SAN2_MSG16P, 14, 'SAN2_MSG16P', has_big5_code=True)
+extract_font(SAN2_NAME16P, 14, 'SAN2_NAME16P')
 # extract_font(SAN3_HAN16P, 14, 'SAN3_HAN16P')
+# extract_font(SAN3_NAME16P, 14, 'SAN3_NAME16P')
 # extract_font(LEMPE_MSG16P, 14, 'LEMPE_MSG16P', has_big5_code=True)
 # extract_font(NOBU4_MSG16P, 14, 'NOBU4_MSG16P')
 # extract_font(SUI_MSG16P, 14, 'SUI_MSG16P', has_big5_code=True)
