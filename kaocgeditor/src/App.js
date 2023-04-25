9fde43935d983674e7682187c4bdaacbd5386f05
Tzeng Yuxio <tzengyuxio@gmail.com>
2023-04-25 21:04:15 +0800

    feat: add GA4

--- kaocgeditor/src/App.js
+++ kaocgeditor/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import ReactGA from 'react-ga4';
 
 import './App.css';
 import Editor from './components/Editor';
+
+ReactGA.initialize('G-1HF0Q4W96B');
 
 function App() {
   return (
